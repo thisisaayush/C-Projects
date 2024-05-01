4d54b1093c909756c9168d338d247a46a6daa9a9
Aayush <athakuri@awtx-itw.com>
2024-04-30 19:41:15 -0500

    resolved push issues

---
+++ C Sharp/C Sharp/Delegates/Addition.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp.Delegates
+{
+    public delegate void Sum(int x, int y);
+
+    public delegate void Display(string a, string y);
+    public class Addition
+    {
+       public static void Main(string[] args)
+        {
+            Addition a = new Addition(); ;
+            Sum sum = new Sum(a.addition);
+            Display display = new Display(a.displayOutcome);
+
+
+            sum(88, 98);
+
+            display("Hello", "Delegates is awesome!");
+        }
+
+        public void addition(int a, int b)
+        {
+            Console.WriteLine( $"Sum: {a + b}");
+        }
+
+        public void displayOutcome(string x, string y)
+        {
+            Console.WriteLine($"Value of x: {x} and Value of y: {y}");
+        }
+    }
+}
